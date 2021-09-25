c09716a8193420e2d091d80ec68903a528e83db2
Mateus Colares <86886637+c0lares@users.noreply.github.com>
2021-09-25 11:27:33 -0300

    Feature/class-endereco (#1)
    * feat: adiciona class de endereço
    
    * feat: adiciona classe endereco

---
+++ poo-projeto-banco/src/main/java/Endereco.java
@@ -0,0 +1,46 @@
+public class Endereco {
+    private String cep;
+    private String estado;
+    private String cidade;
+    private String endereco;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return this.getCep() + ", "
+                + this.getEstado() + ", "
+                + this.getCidade() + '\n'
+                + this.getEndereco();
+    }
+}
