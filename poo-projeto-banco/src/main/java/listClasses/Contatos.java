1ed6b4848a2b83429031b0d9057d88b85e44e846
Mateus Colares <86886637+c0lares@users.noreply.github.com>
2021-10-03 10:30:54 -0300

    Feature/class transacao (#14)
    * fix: attribute ddd deleted
    
    * feat: method menuOperadoras()...fix: toString() with one more \n
    
    * feat: class contatos, list of contato added
    
    * feat: package for list classes added
    
    * feat: class transacao included, need to be updated when class conta be added

---
+++ poo-projeto-banco/src/main/java/listClasses/Contatos.java
@@ -0,0 +1,60 @@
+package listClasses;
+
+import model.Contato;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Contatos{
+    private static final List<Contato>contatos = new ArrayList<>();
+
+    public static List<Contato> getContatos() {
+        return contatos;
+    }
+
+    public static boolean add(Contato a) {
+        for(Contato c : contatos){
+            if(c.getNumero().equals(a.getNumero())){
+                System.out.println("Erro! Contato ja cadastrado");
+                return false;
+            }
+        }
+        contatos.add(a);
+        System.out.println("Contato cadastrado com sucesso");
+        return true;
+    }
+
+    public static boolean delete(String numero, int ddd) {
+        for(Contato c : contatos){
+            if(c.getNumero().equals(numero)){
+                contatos.remove(c);
+                System.out.println("Contato removido:");
+                c.toString();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void read() {
+        System.out.println("\n\n **Contatos cadastrados na sua conta** \n");
+        contatos.stream().peek(System.out::println).collect(Collectors.toList());
+    }
+
+    public static boolean update(String numero) {
+        Scanner scanner = new Scanner(System.in);
+        int novosDados;
+        for(Contato c : contatos){
+            if(c.getNumero().equals(numero)){
+                System.out.println("\nDigite a operadora certa:");
+                c.menuOperadoras();
+                novosDados = scanner.nextInt();
+                c.setOperadora(novosDados);
+            }
+        }
+        return false;
+    }
+
+}
