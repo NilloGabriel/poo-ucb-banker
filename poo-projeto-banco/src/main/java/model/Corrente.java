ad94a9b4d3ccce8ca0676555569804a41461340c
Mateus Colares <mcolaresc@gmail.com>
2021-10-04 14:53:33 -0300

    feat and fix: addind more resources in Conta

--- poo-projeto-banco/src/main/java/model/Corrente.java
+++ poo-projeto-banco/src/main/java/model/Corrente.java
@@ -5,7 +5,7 @@ import listClasses.Transacoes;
 import java.util.Date;
 
 public class Corrente extends Conta{
-    public Corrente(int numeroConta, boolean situacao, Agencia agencia, Date dataCriacao, Double saldo, int numeroBanco, Cartao cartao, Transacoes transacoes, boolean status) {
-        super(numeroConta, situacao, agencia, dataCriacao, saldo, numeroBanco, cartao, transacoes, status);
+    public Corrente(double ganhoMensal,int numeroAgencia, Endereco enderecoAgencia, int numeroConta, boolean situacao, Agencia agencia, Date dataCriacao, Double saldo, int numeroBanco, Cartao cartao, Transacoes transacoes, boolean status) {
+        super(ganhoMensal,numeroAgencia, enderecoAgencia, numeroConta, situacao, dataCriacao, saldo, numeroBanco, cartao, transacoes, status);
     }
 }
--- poo-projeto-banco/src/main/java/model/Poupanca.java
+++ poo-projeto-banco/src/main/java/model/Poupanca.java
@@ -5,7 +5,7 @@ import listClasses.Transacoes;
 import java.util.Date;
 
 public class Poupanca extends Conta{
-    public Poupanca(int numeroConta, boolean situacao, Agencia agencia, Date dataCriacao, Double saldo, int numeroBanco, Cartao cartao, Transacoes transacoes, boolean status) {
-        super(numeroConta, situacao, agencia, dataCriacao, saldo, numeroBanco, cartao, transacoes, status);
+    public Poupanca(double ganhoMensal,int numeroAgencia, Endereco enderecoAgencia, int numeroConta, boolean situacao, Agencia agencia, Date dataCriacao, Double saldo, int numeroBanco, Cartao cartao, Transacoes transacoes, boolean status) {
+        super(ganhoMensal,numeroAgencia, enderecoAgencia, numeroConta, situacao, dataCriacao, saldo, numeroBanco, cartao, transacoes, status);
     }
 }
