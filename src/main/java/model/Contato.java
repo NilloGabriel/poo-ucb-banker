6257dd00feed48e77f6ff608dca9f796c72b9071
Mateus Colares <mcolaresc@gmail.com>
2021-11-24 15:41:30 -0300

    fix: classes from hibernate

---
+++ src/main/java/model/Contato.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author mateu
+ */
+@Entity
+@Table(name = "contato")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Contato.findAll", query = "SELECT c FROM Contato c"),
+    @NamedQuery(name = "Contato.findById", query = "SELECT c FROM Contato c WHERE c.id = :id"),
+    @NamedQuery(name = "Contato.findByNumero", query = "SELECT c FROM Contato c WHERE c.numero = :numero"),
+    @NamedQuery(name = "Contato.findByOperadora", query = "SELECT c FROM Contato c WHERE c.operadora = :operadora")})
+public class Contato implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "numero")
+    private String numero;
+    @Basic(optional = false)
+    @Column(name = "operadora")
+    private String operadora;
+    @JoinColumn(name = "cliente_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Cliente clienteId;
+
+    public Contato() {
+    }
+
+    public Contato(Integer id) {
+        this.id = id;
+    }
+
+    public Contato(Integer id, String numero, String operadora) {
+        this.id = id;
+        this.numero = numero;
+        this.operadora = operadora;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getOperadora() {
+        return operadora;
+    }
+
+    public void setOperadora(String operadora) {
+        this.operadora = operadora;
+    }
+
+    public Cliente getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(Cliente clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Contato)) {
+            return false;
+        }
+        Contato other = (Contato) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Contato[ id=" + id + " ]";
+    }
+    
+}
