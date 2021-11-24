f6e3ef78f4527be718b292d307763a37cfad1a86
Mateus Colares <mcolaresc@gmail.com>
2021-11-24 11:18:53 -0300

    feat: classes from hibernate

---
+++ src/main/java/model/Cliente.java
@@ -0,0 +1,240 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author mateu
+ */
+@Entity
+@Table(name = "cliente")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Cliente.findAll", query = "SELECT c FROM Cliente c"),
+    @NamedQuery(name = "Cliente.findById", query = "SELECT c FROM Cliente c WHERE c.id = :id"),
+    @NamedQuery(name = "Cliente.findByCpf", query = "SELECT c FROM Cliente c WHERE c.cpf = :cpf"),
+    @NamedQuery(name = "Cliente.findByNome", query = "SELECT c FROM Cliente c WHERE c.nome = :nome"),
+    @NamedQuery(name = "Cliente.findByNascimento", query = "SELECT c FROM Cliente c WHERE c.nascimento = :nascimento"),
+    @NamedQuery(name = "Cliente.findByComprovanteResidencia", query = "SELECT c FROM Cliente c WHERE c.comprovanteResidencia = :comprovanteResidencia"),
+    @NamedQuery(name = "Cliente.findByRg", query = "SELECT c FROM Cliente c WHERE c.rg = :rg"),
+    @NamedQuery(name = "Cliente.findByEmailLogin", query = "SELECT c FROM Cliente c WHERE c.emailLogin = :emailLogin"),
+    @NamedQuery(name = "Cliente.findBySenhaLogin", query = "SELECT c FROM Cliente c WHERE c.senhaLogin = :senhaLogin"),
+    @NamedQuery(name = "Cliente.findByLogStatuslogin", query = "SELECT c FROM Cliente c WHERE c.logStatuslogin = :logStatuslogin")})
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "cpf")
+    private String cpf;
+    @Basic(optional = false)
+    @Column(name = "nome")
+    private String nome;
+    @Basic(optional = false)
+    @Column(name = "nascimento")
+    @Temporal(TemporalType.DATE)
+    private Date nascimento;
+    @Basic(optional = false)
+    @Column(name = "comprovanteResidencia")
+    private short comprovanteResidencia;
+    @Basic(optional = false)
+    @Column(name = "rg")
+    private String rg;
+    @Basic(optional = false)
+    @Column(name = "email_login")
+    private String emailLogin;
+    @Basic(optional = false)
+    @Column(name = "senha_login")
+    private String senhaLogin;
+    @Basic(optional = false)
+    @Column(name = "logStatus_login")
+    private short logStatuslogin;
+    @JoinColumn(name = "corrente_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Corrente correnteId;
+    @JoinColumn(name = "poupanca_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Poupanca poupancaId;
+    @OneToMany(mappedBy = "clienteId")
+    private List<Endereco> enderecoList;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "clienteId")
+    private List<Contato> contatoList;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id) {
+        this.id = id;
+    }
+
+    public Cliente(Integer id, String cpf, String nome, Date nascimento, short comprovanteResidencia, String rg, String emailLogin, String senhaLogin, short logStatuslogin) {
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.comprovanteResidencia = comprovanteResidencia;
+        this.rg = rg;
+        this.emailLogin = emailLogin;
+        this.senhaLogin = senhaLogin;
+        this.logStatuslogin = logStatuslogin;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public short getComprovanteResidencia() {
+        return comprovanteResidencia;
+    }
+
+    public void setComprovanteResidencia(short comprovanteResidencia) {
+        this.comprovanteResidencia = comprovanteResidencia;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getEmailLogin() {
+        return emailLogin;
+    }
+
+    public void setEmailLogin(String emailLogin) {
+        this.emailLogin = emailLogin;
+    }
+
+    public String getSenhaLogin() {
+        return senhaLogin;
+    }
+
+    public void setSenhaLogin(String senhaLogin) {
+        this.senhaLogin = senhaLogin;
+    }
+
+    public short getLogStatuslogin() {
+        return logStatuslogin;
+    }
+
+    public void setLogStatuslogin(short logStatuslogin) {
+        this.logStatuslogin = logStatuslogin;
+    }
+
+    public Corrente getCorrenteId() {
+        return correnteId;
+    }
+
+    public void setCorrenteId(Corrente correnteId) {
+        this.correnteId = correnteId;
+    }
+
+    public Poupanca getPoupancaId() {
+        return poupancaId;
+    }
+
+    public void setPoupancaId(Poupanca poupancaId) {
+        this.poupancaId = poupancaId;
+    }
+
+    @XmlTransient
+    public List<Endereco> getEnderecoList() {
+        return enderecoList;
+    }
+
+    public void setEnderecoList(List<Endereco> enderecoList) {
+        this.enderecoList = enderecoList;
+    }
+
+    @XmlTransient
+    public List<Contato> getContatoList() {
+        return contatoList;
+    }
+
+    public void setContatoList(List<Contato> contatoList) {
+        this.contatoList = contatoList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "dao.Cliente[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/main/java/model/Corrente.java
@@ -0,0 +1,190 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author mateu
+ */
+@Entity
+@Table(name = "corrente")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Corrente.findAll", query = "SELECT c FROM Corrente c"),
+    @NamedQuery(name = "Corrente.findById", query = "SELECT c FROM Corrente c WHERE c.id = :id"),
+    @NamedQuery(name = "Corrente.findByNumero", query = "SELECT c FROM Corrente c WHERE c.numero = :numero"),
+    @NamedQuery(name = "Corrente.findByStatus", query = "SELECT c FROM Corrente c WHERE c.status = :status"),
+    @NamedQuery(name = "Corrente.findByDataCriacao", query = "SELECT c FROM Corrente c WHERE c.dataCriacao = :dataCriacao"),
+    @NamedQuery(name = "Corrente.findBySaldo", query = "SELECT c FROM Corrente c WHERE c.saldo = :saldo"),
+    @NamedQuery(name = "Corrente.findByGanhoMensal", query = "SELECT c FROM Corrente c WHERE c.ganhoMensal = :ganhoMensal")})
+public class Corrente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "numero")
+    private String numero;
+    @Basic(optional = false)
+    @Column(name = "status")
+    private short status;
+    @Basic(optional = false)
+    @Column(name = "data_criacao")
+    @Temporal(TemporalType.DATE)
+    private Date dataCriacao;
+    @Basic(optional = false)
+    @Column(name = "saldo")
+    private long saldo;
+    @Basic(optional = false)
+    @Column(name = "ganhoMensal")
+    private long ganhoMensal;
+    @JoinColumn(name = "agencia_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Agencia agenciaId;
+    @JoinColumn(name = "cartao_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Cartao cartaoId;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "correnteId")
+    private List<Cliente> clienteList;
+
+    public Corrente() {
+    }
+
+    public Corrente(Integer id) {
+        this.id = id;
+    }
+
+    public Corrente(Integer id, String numero, short status, Date dataCriacao, long saldo, long ganhoMensal) {
+        this.id = id;
+        this.numero = numero;
+        this.status = status;
+        this.dataCriacao = dataCriacao;
+        this.saldo = saldo;
+        this.ganhoMensal = ganhoMensal;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public short getStatus() {
+        return status;
+    }
+
+    public void setStatus(short status) {
+        this.status = status;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public long getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(long saldo) {
+        this.saldo = saldo;
+    }
+
+    public long getGanhoMensal() {
+        return ganhoMensal;
+    }
+
+    public void setGanhoMensal(long ganhoMensal) {
+        this.ganhoMensal = ganhoMensal;
+    }
+
+    public Agencia getAgenciaId() {
+        return agenciaId;
+    }
+
+    public void setAgenciaId(Agencia agenciaId) {
+        this.agenciaId = agenciaId;
+    }
+
+    public Cartao getCartaoId() {
+        return cartaoId;
+    }
+
+    public void setCartaoId(Cartao cartaoId) {
+        this.cartaoId = cartaoId;
+    }
+
+    @XmlTransient
+    public List<Cliente> getClienteList() {
+        return clienteList;
+    }
+
+    public void setClienteList(List<Cliente> clienteList) {
+        this.clienteList = clienteList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Corrente)) {
+            return false;
+        }
+        Corrente other = (Corrente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "dao.Corrente[ id=" + id + " ]";
+    }
+    
+}
